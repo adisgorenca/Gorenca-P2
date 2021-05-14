652223a90ab375ca3a435cc898e75b188fbd4e89
adisgorenca <50460660+adisgorenca@users.noreply.github.com>
2021-05-14 13:28:40 -0400

    O

--- js/gallery.js
+++ js/gallery.js
@@ -32,15 +32,6 @@ function animate() {
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
-function rotate(){
-	if ($(".moreIndicator").hasClass("rot90")) {
-		$(".moreIndicator").removeClass("rot90").addClass("rot270");
-		$(".details").slideToggle( "slow" );
-	} else {
-		$(".moreIndicator").removeClass("rot270").addClass("rot90");
-		$(".details").slideToggle( "slow" );
-	}
-};
 
 function swapPhoto() {
 	if(mCurrentIndex >= mImages.length) {
@@ -143,7 +134,15 @@ window.addEventListener('load', function() {
 
 }, false);
 
-
+function rotate(){
+	if ($(".moreIndicator").hasClass("rot90")) {
+		$(".moreIndicator").removeClass("rot90").addClass("rot270");
+		$(".details").slideToggle( "slow" );
+	} else {
+		$(".moreIndicator").removeClass("rot270").addClass("rot90");
+		$(".details").slideToggle( "slow" );
+	}
+};
 
 function GalleryImage() {
 	var location;
